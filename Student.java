7d6e6015d8a875a0bcce4f80ce0186843b41b05b
Andrew Gravino <aggravino@cpp.edu>
2022-09-28 11:39:11 -0700

    Added Setter/Getter methods for the Student class' private variables

--- Student.java
+++ Student.java
@@ -8,3 +8,33 @@ September 29, 2022
 Understand:
     This class exists to store the unique ID's and a person's answers
  */
+
+public class Student{
+
+    private String uniqueID;
+    private char singleChoiceAnswer;
+    private String multipleChoiceAnswer;
+
+    public void Student(String inputID, String inputMultipleChoice){
+        uniqueID = inputID;
+        multipleChoiceAnswer = inputMultipleChoice;
+    }
+
+    public void Student(String inputID, char inputSingleChoice){
+        uniqueID = inputID;
+        singleChoiceAnswer = inputSingleChoice;
+    }
+
+    public String getUniqueID(){
+        return uniqueID;
+    }
+
+    public String getSingleChoiceAnswer(){
+        return singleChoiceAnswer;
+    }
+
+    public String getMultipleChoiceAnswer(){
+        return multipleChoiceAnswer;
+    }
+
+}
